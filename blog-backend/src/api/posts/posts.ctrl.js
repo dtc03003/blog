f831c4a730c94f3220a50467b81d6a2254bb68d1
김동현 <dtc03003@naver.com>
2023-03-31 22:50:19 +0900

    username/tags로 필터

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -68,6 +68,13 @@ export const list = async (ctx) => {
         return;
     }
 
+    const { tag, username } = ctx.query;
+    // tag, username 값이 유효하면 객체 안에 넣고, 그렇지 않으면 넣지 않음
+    const query = {
+        ...(username ? { 'user.username': username } : {}),
+        ...(tag ? { tags: tag } : {}),
+    };
+
     try {
         const posts = await Post.find()
             .sort({ _id: -1 })
