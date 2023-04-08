43ff7080b5b020fdb19db89f2f39dbe07ffd525d
김동현 <dtc03003@naver.com>
2023-04-08 23:14:34 +0900

    input 값 안써지는 에러

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -11,43 +11,52 @@ const RegisterForm = () => {
         authError: auth.authError,
     }));
     // 인풋 변경 이벤트 핸들러
-    // const onChange = (e) => {};
+    const onChange = (e) => {
+        const { value, name } = e.target;
+        dispatch(
+            changeField({
+                form: 'register',
+                key: name,
+                value,
+            }),
+        );
+    };
 
-    // // 폼 등록 이벤트 핸들러
-    // const onSubmit = (e) => {
-    //     e.preventDefault();
-    //     const { username, password, passwordConfirm } = form;
-    //     if (password !== passwordConfirm) {
-    //         // TODO: 오류 처리
-    //         return;
-    //     }
-    //     dispatch(register({ username, password }));
-    // };
+    // 폼 등록 이벤트 핸들러
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const { username, password, passwordConfirm } = form;
+        if (password !== passwordConfirm) {
+            // TODO: 오류 처리
+            return;
+        }
+        dispatch(register({ username, password }));
+    };
 
-    // // 컴포넌트가 처음 렌더링될 때 form을 초기화함
-    // useEffect(() => {
-    //     dispatch(initializeForm('register'));
-    // }, [dispatch]);
+    // 컴포넌트가 처음 렌더링될 때 form을 초기화함
+    useEffect(() => {
+        dispatch(initializeForm('register'));
+    }, [dispatch]);
 
-    // // 회원가입 성공/실패 처리
-    // useEffect(() => {
-    //     if (authError) {
-    //         console.log('오류 발생');
-    //         console.log(authError);
-    //         return;
-    //     }
-    //     if (auth) {
-    //         console.log('회원가입 성공');
-    //         console.log(auth);
-    //     }
-    // }, [auth, authError]);
+    // 회원가입 성공/실패 처리
+    useEffect(() => {
+        if (authError) {
+            console.log('오류 발생');
+            console.log(authError);
+            return;
+        }
+        if (auth) {
+            console.log('회원가입 성공');
+            console.log(auth);
+        }
+    }, [auth, authError]);
 
     return (
         <AuthForm
             type="register"
             form={form}
-            // onChange={onChange}
-            // onSubmit={onSubmit}
+            onChange={onChange}
+            onSubmit={onSubmit}
         />
     );
 };
