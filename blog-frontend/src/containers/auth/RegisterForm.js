c84911d385f0fe1fb9107a37684277215afc7275
김동현 <dtc03003@naver.com>
2023-04-10 20:37:55 +0900

    로그인 상태 유지

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -80,6 +80,11 @@ const RegisterForm = () => {
     useEffect(() => {
         if (user) {
             navigate('/'); // 홈 화면으로 이동
+            try {
+                localStorage.setItem('user', JSON.stringify(user));
+            } catch (e) {
+                console.log('localStorage is not working');
+            }
         }
     }, [navigate, user]);
 
