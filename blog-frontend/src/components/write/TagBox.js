d5e0a81ffe58112218ac4cf81a86c051d2c9556a
김동현 <dtc03003@naver.com>
2023-04-11 22:35:11 +0900

    tagboxcontainer 만들기

--- blog-frontend/src/components/write/TagBox.js
+++ blog-frontend/src/components/write/TagBox.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 
@@ -74,7 +74,7 @@ const TagList = React.memo(({ tags, onRemove }) => (
     </TagListBlock>
 ));
 
-const TagBox = () => {
+const TagBox = ({ tags, onChangeTags }) => {
     const [input, setInput] = useState('');
     const [localTags, setLocalTags] = useState([]);
 
@@ -82,16 +82,20 @@ const TagBox = () => {
         (tag) => {
             if (!tag) return; // 공백이라면 추가하지 않음
             if (localTags.includes(tag)) return; // 이미 존재한다면 추가하지 않음
-            setLocalTags([...localTags, tag]);
+            const nextTags = [...localTags, tag];
+            setLocalTags(nextTags);
+            onChangeTags(nextTags);
         },
-        [localTags],
+        [localTags, onChangeTags],
     );
 
     const onRemove = useCallback(
         (tag) => {
-            setLocalTags(localTags.filter((t) => t !== tag));
+            const nextTags = localTags.filter((t) => t !== tag);
+            setLocalTags(nextTags);
+            onChangeTags(nextTags);
         },
-        [localTags],
+        [localTags, onChangeTags],
     );
 
     const onChange = useCallback((e) => {
@@ -106,6 +110,12 @@ const TagBox = () => {
         },
         [input, insertTag],
     );
+
+    // tags 값이 바뀔 때
+    useEffect(() => {
+        setLocalTags(tags);
+    }, [tags]);
+
     return (
         <TagBoxBlock>
             <h4>태그</h4>
--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -3,12 +3,13 @@ import TagBox from '../components/write/TagBox';
 import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
 import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
 
 const WritePage = () => {
     return (
         <Responsive>
             <EditorContainer />
-            <TagBox />
+            <TagBoxContainer />
             <WriteActionButtons />
         </Responsive>
     );
