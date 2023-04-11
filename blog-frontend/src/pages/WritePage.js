8eb5f9622bacaedd4432b6a11e47003710368008
김동현 <dtc03003@naver.com>
2023-04-11 22:57:09 +0900

    글쓰기 API 연동

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import TagBox from '../components/write/TagBox';
-import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
 import EditorContainer from '../containers/write/EditorContainer';
 import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 
 const WritePage = () => {
     return (
         <Responsive>
             <EditorContainer />
             <TagBoxContainer />
-            <WriteActionButtons />
+            <WriteActionButtonsContainer />
         </Responsive>
     );
 };
