f0548a4db21ce6ea2a8f251ee763cf7fd2e06c85
김동현 <dtc03003@naver.com>
2023-04-11 22:12:55 +0900

    editorcontainer 만들기

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Editor from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
+import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
+import EditorContainer from '../containers/write/EditorContainer';
 
 const WritePage = () => {
     return (
         <Responsive>
-            <Editor />
+            <EditorContainer />
+            <TagBox />
+            <WriteActionButtons />
         </Responsive>
     );
 };
