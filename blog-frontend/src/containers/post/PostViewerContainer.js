af00ddac42a47d9124fbdb3c19da423c0572ab58
김동현 <dtc03003@naver.com>
2023-04-15 23:36:50 +0900

    포스터 수정 기능

--- blog-frontend/src/containers/post/PostViewerContainer.js
+++ blog-frontend/src/containers/post/PostViewerContainer.js
@@ -2,17 +2,23 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { readPost, unloadPost } from '../../modules/post';
 import PostViewer from '../../components/post/PostViewer';
-import { useParams } from '../../../node_modules/react-router-dom/dist/index';
+import PostActionButtons from '../../components/post/PostActionButtons';
+import { setOriginalPost } from '../../modules/write';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const PostViewerContainer = ({ match }) => {
     // 처음 마운트될 때 포스트 읽기 API 요청
     const { postId } = useParams();
+    const navigate = useNavigate();
     const dispatch = useDispatch();
-    const { post, error, loading } = useSelector(({ post, loading }) => ({
-        post: post.post,
-        error: post.error,
-        loading: loading['post/READ_POST'],
-    }));
+    const { post, error, loading, user } = useSelector(
+        ({ post, loading, user }) => ({
+            post: post.post,
+            error: post.error,
+            loading: loading['post/READ_POST'],
+            user: user.user,
+        }),
+    );
 
     useEffect(() => {
         dispatch(readPost(postId));
@@ -22,7 +28,21 @@ const PostViewerContainer = ({ match }) => {
         };
     }, [dispatch, postId]);
 
-    return <PostViewer post={post} loading={loading} error={error} />;
+    const onEdit = () => {
+        dispatch(setOriginalPost(post));
+        navigate('/write');
+    };
+
+    const ownPost = (user && user._id) === (post && post.user._id);
+
+    return (
+        <PostViewer
+            post={post}
+            loading={loading}
+            error={error}
+            actionButtons={ownPost && <PostActionButtons onEdit={onEdit} />}
+        />
+    );
 };
 
 export default PostViewerContainer;
