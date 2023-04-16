4ac17d878a1aff5bc514ca62a5a77630c7c9a6d0
김동현 <dtc03003@naver.com>
2023-04-16 20:50:00 +0900

    react-helmet-async로 meta 설정

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -5,19 +5,27 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import WritePage from './pages/WritePage';
 import PostPage from './pages/PostPage';
+import { Helmet } from 'react-helmet-async';
 
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<PostListPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/write" element={<WritePage />} />
-            <Route path="/:username">
-                <Route index element={<PostListPage />} />
-                <Route path=":postId" element={<PostPage />} />
-            </Route>
-        </Routes>
+        <>
+            <>
+                <Helmet>
+                    <title>REACTERS</title>
+                </Helmet>
+            </>
+            <Routes>
+                <Route path="/" element={<PostListPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/write" element={<WritePage />} />
+                <Route path="/:username">
+                    <Route index element={<PostListPage />} />
+                    <Route path=":postId" element={<PostPage />} />
+                </Route>
+            </Routes>
+        </>
     );
 };
 export default App;
